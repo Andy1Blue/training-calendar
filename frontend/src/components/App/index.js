3ea8be2b6f95a4c35aa1f273c1d6fefda8b8137f
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-05-17 20:25:59 +0200

    Adding toast div with editing

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -15,7 +15,22 @@ class App extends Component {
     state = {
         isFetching: false,
         isLogin: false, // Local Sotorage TCgId exist
-        TCgId: null
+        TCgId: null,
+        showDay: false,
+        targetDay: null
+    }
+
+    // Show the day editing panel
+    showDay = (e) => {
+        if (e.target.attributes.getNamedItem('id') !== null) {
+            console.log("Clicked" + e.target.attributes.getNamedItem('id').value);
+            this.setState({ showDay: true, targetDay: e.target.attributes.getNamedItem('id').value });
+        }
+    }
+
+    // Close the day editing panel
+    closeDay = () => {
+        this.setState({ showDay: false });
     }
 
     componentDidMount() {
@@ -29,25 +44,40 @@ class App extends Component {
     }
 
     render() {
-        const { isFetching, TCgId } = this.state;
+        const { isFetching, TCgId, showDay, targetDay } = this.state;
         return (
             <div className="App">
-            {isFetching && <div>Loader</div>}
-            {TCgId === null && !isFetching &&
-             <header className="App-header">
-             <div>
-                 Training calendar
-              <div>
-                     <GoogleLogin />
-                 </div>
-             </div>
-         </header>
-            }
-            {TCgId != null && !isFetching && <div>
-             <div>
-                <GoogleLogin />
-                </div>
-                <ListOfMonths TCgId={TCgId} />
+                {isFetching && <div>Loader</div>}
+                {TCgId === null && !isFetching &&
+                    <header className="App-header">
+                        <div>
+                            Training calendar
+                            <div>
+                                <GoogleLogin />
+                            </div>
+                        </div>
+                    </header>
+                }
+                {TCgId != null && !isFetching && <div>
+                    <div>
+                        <GoogleLogin />
+                    </div>
+
+                    {showDay &&
+                        <div id="red-toast">
+                            <div>
+                                <button id="red-toast-close" onClick={this.closeDay}>x</button>
+                                Day: {targetDay}
+                                <br />Workout? <input type="checkbox"></input>
+                                <br />Comment:<br /><textarea></textarea>
+                                <br /><button>Save</button>
+                            </div>
+                        </div>
+                    }
+
+                    <div onClick={this.showDay}>
+                        <ListOfMonths TCgId={TCgId} />
+                    </div>
                 </div>
                 }
             </div>
